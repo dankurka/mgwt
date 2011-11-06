7c9fdd95afc4136fa4df92b771c839b31776abd6
Daniel Kurka <kurt@ncco.de>
2011-11-06 21:27:53 +0100

    javadoc

--- src/main/java/com/googlecode/mgwt/ui/client/MGWTStyle.java
+++ src/main/java/com/googlecode/mgwt/ui/client/MGWTStyle.java
@@ -4,9 +4,26 @@ import com.google.gwt.core.client.GWT;
 import com.googlecode.mgwt.ui.client.theme.base.MGWTClientBundle;
 
 /**
- * <p>
- * MGWTStyle class.
- * </p>
+ * The {@link MGWTStyle} class provides an easy access to the default theme of
+ * every gwt app.
+ * 
+ * If widgets are created without a specific theme they ask MGWTStyle for the
+ * default theme.
+ * 
+ * The default theme can be changed at start up time of an mgwt app <b>once</b>.
+ * 
+ * After startup the bundle can not be changed. If no bundle is set, MGWTStyle
+ * creates the MGWT Default theme.
+ * 
+ * You can change the theme like this:
+ * 
+ * <pre>
+ * // create your bundle
+ * MGWTClientBundle bundle = GWT.create(YourBundle.class);
+ * // set your bundle as default
+ * MGWTStyle.setDefaultBundle(bundle);
+ * </pre>
+ * 
  * 
  * @author Daniel Kurka
  * @version $Id: $
@@ -45,6 +62,7 @@ public class MGWTStyle {
 		if (defaultClientBundle != null) {
 			throw new IllegalStateException("can not change default bundle if theres already an instance...");
 		}
+		bundle.getMainCss().ensureInjected();
 		defaultClientBundle = bundle;
 	}
 }
