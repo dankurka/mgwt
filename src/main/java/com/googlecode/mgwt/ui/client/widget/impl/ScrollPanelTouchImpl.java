496d1ca97e4cef56c6eeb3738ffa85110f8b2c1a
Daniel Kurka <kurt@ncco.de>
2011-12-07 13:51:50 +0100

    scrollpanel fixes for ff

--- src/main/java/com/googlecode/mgwt/ui/client/widget/impl/ScrollPanelTouchImpl.java
+++ src/main/java/com/googlecode/mgwt/ui/client/widget/impl/ScrollPanelTouchImpl.java
@@ -289,7 +289,7 @@ public class ScrollPanelTouchImpl extends ScrollPanelImpl {
 	private final class MouseWheelHandlerImplementation implements MouseWheelHandler {
 		@Override
 		public void onMouseWheel(MouseWheelEvent event) {
-
+			System.out.println("mousewheel");
 			int velocityX = 0;
 			int velocityY = 0;
 
@@ -346,7 +346,9 @@ public class ScrollPanelTouchImpl extends ScrollPanelImpl {
 	}-*/;
 
 	private native int getMouseWheelVelocityY(NativeEvent evt)/*-{
-		return Math.round(-evt.wheelDeltaY) || 0;
+
+		var val = (evt.detail * 40) || -evt.wheelDeltaY || 0;
+		return Math.round(val);
 	}-*/;
 
 	/** {@inheritDoc} */
