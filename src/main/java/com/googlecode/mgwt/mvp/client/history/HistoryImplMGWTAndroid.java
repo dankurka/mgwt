2b239aa9dcacde0d7de45f0da7a3760aac12c6ab
Daniel Kurka <kurt@ncco.de>
2012-01-12 18:29:19 +0100

    fixing backbutton delay on android

---
+++ src/main/java/com/googlecode/mgwt/mvp/client/history/HistoryImplMGWTAndroid.java
@@ -0,0 +1,10 @@
+package com.googlecode.mgwt.mvp.client.history;
+
+import com.google.gwt.user.client.impl.HistoryImpl;
+
+public class HistoryImplMGWTAndroid extends HistoryImpl {
+
+	public HistoryImplMGWTAndroid() {
+
+	}
+}
