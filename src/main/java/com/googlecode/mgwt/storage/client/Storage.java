35458ea14cde6e7f0da6ae7bbe48ac9971d0afee
Daniel Kurka <kurka.daniel@googlemail.com>
2012-04-24 07:31:41 +0200

    updated javadoc

--- src/main/java/com/googlecode/mgwt/storage/client/Storage.java
+++ src/main/java/com/googlecode/mgwt/storage/client/Storage.java
@@ -5,12 +5,14 @@ package com.googlecode.mgwt.storage.client;
  * 
  * This exists for testing reasons & use with dependency injection (GIN).
  * 
- * There is an implementation that just uses GWT Implementation of LocalStorage @see {@link LocalStorageGwtImpl} and another one that can be used for testing @see {@link LocalStorageImplForTests}
+ * There is an implementation that just uses GWT Implementation of LocalStorage @see
+ * {@link LocalStorageGwtImpl} and another one that can be used for testing @see
+ * {@link LocalStorageImplForTests}
  * 
  * 
  * 
  * @author Daniel Kurka
- *
+ * 
  */
 public interface Storage {
 
@@ -22,7 +24,7 @@ public interface Storage {
 
 	/**
 	 * Removes all items in the Storage.
-	 *
+	 * 
 	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-clear">W3C Web
 	 *      Storage - Storage.clear()</a>
 	 */
@@ -30,17 +32,18 @@ public interface Storage {
 
 	/**
 	 * Returns the item in the Storage associated with the specified key.
-	 *
+	 * 
 	 * @param key the key to a value in the Storage
-	 * @return the value associated with the given key
-	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-getitem">W3C Web
-	 *      Storage - Storage.getItem(k)</a>
+	 * @return the value associated with the given key, if there is no key it
+	 *         returns null
+	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-getitem">W3C
+	 *      Web Storage - Storage.getItem(k)</a>
 	 */
 	public String getItem(String key);
 
 	/**
 	 * Returns the number of items in this Storage.
-	 *
+	 * 
 	 * @return number of items in this Storage
 	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-l">W3C Web
 	 *      Storage - Storage.length()</a>
@@ -49,7 +52,7 @@ public interface Storage {
 
 	/**
 	 * Returns the key at the specified index.
-	 *
+	 * 
 	 * @param index the index of the key
 	 * @return the key at the specified index in this Storage
 	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-key">W3C Web
@@ -59,9 +62,10 @@ public interface Storage {
 
 	/**
 	 * Removes the item in the Storage associated with the specified key.
-	 *
+	 * 
 	 * @param key the key to a value in the Storage
-	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-removeitem">W3C
+	 * @see <a
+	 *      href="http://www.w3.org/TR/webstorage/#dom-storage-removeitem">W3C
 	 *      Web Storage - Storage.removeItem(k)</a>
 	 */
 	public void removeItem(String key);
@@ -69,13 +73,13 @@ public interface Storage {
 	/**
 	 * Sets the value in the Storage associated with the specified key to the
 	 * specified data.
-	 *
+	 * 
 	 * Note: The empty string may not be used as a key.
-	 *
+	 * 
 	 * @param key the key to a value in the Storage
 	 * @param data the value associated with the key
-	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-setitem">W3C Web
-	 *      Storage - Storage.setItem(k,v)</a>
+	 * @see <a href="http://www.w3.org/TR/webstorage/#dom-storage-setitem">W3C
+	 *      Web Storage - Storage.setItem(k,v)</a>
 	 */
 	public void setItem(String key, String data);
 }
