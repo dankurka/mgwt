bd96bd4fe12240ef7f0732b0555c43bfcd9802bd
Daniel Kurka <kurka.daniel@googlemail.com>
2012-06-18 23:51:18 +0200

    fixes ISSUE 193

--- src/main/java/com/googlecode/mgwt/mvp/client/display/AnimatableDisplayBaseImpl.java
+++ src/main/java/com/googlecode/mgwt/mvp/client/display/AnimatableDisplayBaseImpl.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.mvp.client.display;
@@ -35,133 +33,132 @@ import com.googlecode.mgwt.mvp.client.resources.AnimationSelector;
  */
 public abstract class AnimatableDisplayBaseImpl implements AnimatableDisplay {
 
-	protected FlowPanel main;
+  protected FlowPanel main;
 
-	protected SimplePanel first;
+  protected SimplePanel first;
 
-	protected SimplePanel second;
+  protected SimplePanel second;
 
-	protected boolean lastDir;
+  protected boolean lastDir;
 
-	protected final AnimationCss css;
+  protected final AnimationCss css;
 
-	/**
-	 * <p>
-	 * Constructor for AnimatableDisplayBaseImpl.
-	 * </p>
-	 */
-	public AnimatableDisplayBaseImpl() {
-		this(AnimationSelector.getBundle().animationCss());
-	}
+  /**
+   * <p>
+   * Constructor for AnimatableDisplayBaseImpl.
+   * </p>
+   */
+  public AnimatableDisplayBaseImpl() {
+    this(AnimationSelector.getBundle().animationCss());
+  }
 
-	/**
-	 * <p>
-	 * Constructor for AnimatableDisplayBaseImpl.
-	 * </p>
-	 * 
-	 * @param css a
-	 *            {@link com.googlecode.mgwt.mvp.client.resources.AnimationCss}
-	 *            object.
-	 */
-	public AnimatableDisplayBaseImpl(AnimationCss css) {
+  /**
+   * <p>
+   * Constructor for AnimatableDisplayBaseImpl.
+   * </p>
+   * 
+   * @param css a {@link com.googlecode.mgwt.mvp.client.resources.AnimationCss} object.
+   */
+  public AnimatableDisplayBaseImpl(AnimationCss css) {
 
-		this.css = css;
-		css.ensureInjected();
+    this.css = css;
+    css.ensureInjected();
 
-		main = new FlowPanel() {
-			protected void onDetach() {
-				onDeattach();
+    main = new FlowPanel() {
+      protected void onDetach() {
+        super.onDetach();
+        onDeattach();
 
-			};
-		};
+      };
+    };
 
-		main.setStylePrimaryName(this.css.display());
+    main.setStylePrimaryName(this.css.display());
 
-		first = new SimplePanel();
-		first.addStyleName(this.css.displayContainer());
+    first = new SimplePanel();
+    first.addStyleName(this.css.displayContainer());
 
-		second = new SimplePanel();
-		second.addStyleName(this.css.displayContainer());
+    second = new SimplePanel();
+    second.addStyleName(this.css.displayContainer());
 
-		listener = new AnimationEndListener();
+    listener = new AnimationEndListener();
 
-	}
+  }
 
-	protected class AnimationEndListener implements AnimationEndHandler {
+  protected class AnimationEndListener implements AnimationEndHandler {
 
-		@Override
-		public void onAnimationEnd(AnimationEndEvent event) {
-			AnimatableDisplayBaseImpl.this.onAnimationEnd();
+    @Override
+    public void onAnimationEnd(AnimationEndEvent event) {
+      AnimatableDisplayBaseImpl.this.onAnimationEnd();
 
-		}
+    }
 
-	}
+  }
 
-	/** {@inheritDoc} */
-	@Override
-	public void setFirstWidget(IsWidget w) {
-		first.setWidget(w);
-	}
+  /** {@inheritDoc} */
+  @Override
+  public void setFirstWidget(IsWidget w) {
+    first.setWidget(w);
+  }
 
-	/** {@inheritDoc} */
-	@Override
-	public void setSecondWidget(IsWidget w) {
-		second.setWidget(w);
-	}
+  /** {@inheritDoc} */
+  @Override
+  public void setSecondWidget(IsWidget w) {
+    second.setWidget(w);
+  }
 
-	/**
-	 * <p>
-	 * removeAllStyles
-	 * </p>
-	 */
-	protected void removeAllStyles() {
+  /**
+   * <p>
+   * removeAllStyles
+   * </p>
+   */
+  protected void removeAllStyles() {
 
-		first.removeStyleName(this.css.in());
-		first.removeStyleName(this.css.out());
-		first.removeStyleName(this.css.reverse());
+    first.removeStyleName(this.css.in());
+    first.removeStyleName(this.css.out());
+    first.removeStyleName(this.css.reverse());
 
-		first.removeStyleName(this.css.dissolve());
-		first.removeStyleName(this.css.fade());
-		first.removeStyleName(this.css.flip());
-		first.removeStyleName(this.css.pop());
-		first.removeStyleName(this.css.slide());
-		first.removeStyleName(this.css.slideup());
-		first.removeStyleName(this.css.swap());
+    first.removeStyleName(this.css.dissolve());
+    first.removeStyleName(this.css.fade());
+    first.removeStyleName(this.css.flip());
+    first.removeStyleName(this.css.pop());
+    first.removeStyleName(this.css.slide());
+    first.removeStyleName(this.css.slideup());
+    first.removeStyleName(this.css.swap());
 
-		second.removeStyleName(this.css.in());
-		second.removeStyleName(this.css.out());
-		second.removeStyleName(this.css.reverse());
+    second.removeStyleName(this.css.in());
+    second.removeStyleName(this.css.out());
+    second.removeStyleName(this.css.reverse());
 
-		second.removeStyleName(this.css.dissolve());
-		second.removeStyleName(this.css.fade());
-		second.removeStyleName(this.css.flip());
-		second.removeStyleName(this.css.pop());
-		second.removeStyleName(this.css.slide());
-		second.removeStyleName(this.css.slideup());
-		second.removeStyleName(this.css.swap());
+    second.removeStyleName(this.css.dissolve());
+    second.removeStyleName(this.css.fade());
+    second.removeStyleName(this.css.flip());
+    second.removeStyleName(this.css.pop());
+    second.removeStyleName(this.css.slide());
+    second.removeStyleName(this.css.slideup());
+    second.removeStyleName(this.css.swap());
 
-	}
+  }
 
-	/**
-	 * <p>
-	 * onAnimationEnd
-	 * </p>
-	 */
-	protected abstract void onAnimationEnd();
+  /**
+   * <p>
+   * onAnimationEnd
+   * </p>
+   */
+  protected abstract void onAnimationEnd();
 
-	protected boolean showFirst;
-	protected HandlerRegistration animationEnd;
+  protected boolean showFirst;
+  protected HandlerRegistration animationEnd;
 
-	protected AnimationEndListener listener;
+  protected AnimationEndListener listener;
 
-	protected AnimationEndCallback lastCallback;
+  protected AnimationEndCallback lastCallback;
 
-	/**
-	 * <p>
-	 * blurBeforeAnimation
-	 * </p>
-	 */
-	protected native void blurBeforeAnimation() /*-{
+  /**
+   * <p>
+   * blurBeforeAnimation
+   * </p>
+   */
+  protected native void blurBeforeAnimation() /*-{
 		var node = $doc.querySelector(":focus");
 
 		if (node != null) {
@@ -170,17 +167,19 @@ public abstract class AnimatableDisplayBaseImpl implements AnimatableDisplay {
 			}
 
 		}
-	}-*/;
+  }-*/;
 
-	/* (non-Javadoc)
-	 * @see com.google.gwt.user.client.ui.IsWidget#asWidget()
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public Widget asWidget() {
-		return main;
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.IsWidget#asWidget()
+   */
+  /** {@inheritDoc} */
+  @Override
+  public Widget asWidget() {
+    return main;
+  }
 
-	abstract protected void onDeattach();
+  abstract protected void onDeattach();
 
 }
