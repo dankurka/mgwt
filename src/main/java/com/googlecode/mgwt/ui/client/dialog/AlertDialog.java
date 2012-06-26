d06dec01f88339cec1dacceb7b42f1b4b7ef1cbf
Daniel Kurka <kurka.daniel@googlemail.com>
2012-06-26 19:21:50 +0200

    added i18n for alert dialog

--- src/main/java/com/googlecode/mgwt/ui/client/dialog/AlertDialog.java
+++ src/main/java/com/googlecode/mgwt/ui/client/dialog/AlertDialog.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.ui.client.dialog;
@@ -31,115 +29,135 @@ import com.googlecode.mgwt.ui.client.theme.base.DialogCss;
  */
 public class AlertDialog implements HasText, HasTitleText, HasTapHandlers, Dialog {
 
-	private Label textLabel;
-	private PopinDialog popinDialog;
-	private DialogPanel dialogPanel1;
+  private Label textLabel;
+  private PopinDialog popinDialog;
+  private DialogPanel dialogPanel1;
 
-	/**
-	 * Construct an alert dialog
-	 * 
-	 * @param title - the title of the dialog
-	 * @param text - the text of the dialog
-	 */
-	public AlertDialog(String title, String text) {
-		this(MGWTStyle.getTheme().getMGWTClientBundle().getDialogCss(), title, text);
-	}
+  /**
+   * Construct an alert dialog
+   * 
+   * @param title - the title of the dialog
+   * @param text - the text of the dialog
+   */
+  public AlertDialog(String title, String text) {
+    this(MGWTStyle.getTheme().getMGWTClientBundle().getDialogCss(), title, text);
+  }
 
-	/**
-	 * Construct an alert dialog
-	 * 
-	 * @param css - the css to use
-	 * @param title - the title of the dialog
-	 * @param text - the text of the dialog
-	 */
-	public AlertDialog(DialogCss css, String title, String text) {
-		css.ensureInjected();
-		popinDialog = new PopinDialog(css);
-		dialogPanel1 = new DialogPanel();
-		dialogPanel1.showCancelButton(false);
-		dialogPanel1.showOkButton(true);
+  /**
+   * Construct an alert dialog
+   * 
+   * @param css - the css to use
+   * @param title - the title of the dialog
+   * @param text - the text of the dialog
+   */
+  public AlertDialog(DialogCss css, String title, String text) {
+    this(css, title, text, "Ok");
+  }
 
-		textLabel = new Label();
-		dialogPanel1.getContent().add(textLabel);
-		popinDialog.add(dialogPanel1);
+  /**
+   * Construct an alert dialog
+   * 
+   * @param css - the css to use
+   * @param title - the title of the dialog
+   * @param text - the text of the dialog
+   */
+  public AlertDialog(DialogCss css, String title, String text, String okButtonText) {
+    css.ensureInjected();
+    popinDialog = new PopinDialog(css);
+    dialogPanel1 = new DialogPanel();
+    dialogPanel1.showCancelButton(false);
+    dialogPanel1.showOkButton(true);
 
-		dialogPanel1.getOkButton().addTapHandler(new TapHandler() {
+    textLabel = new Label();
+    dialogPanel1.getContent().add(textLabel);
+    popinDialog.add(dialogPanel1);
 
-			@Override
-			public void onTap(TapEvent event) {
-				popinDialog.hide();
-			}
+    dialogPanel1.getOkButton().addTapHandler(new TapHandler() {
 
-		});
+      @Override
+      public void onTap(TapEvent event) {
+        popinDialog.hide();
+      }
 
-		setText(text);
-		setTitleText(title);
+    });
 
-	}
+    setText(text);
+    setTitleText(title);
+    dialogPanel1.setOkButtonText(okButtonText);
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.google.gwt.user.client.ui.HasText#getText()
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public String getText() {
-		return textLabel.getText();
-	}
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.google.gwt.user.client.ui.HasText#setText(java.lang.String)
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public void setText(String text) {
-		textLabel.setText(text);
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasText#getText()
+   */
+  /** {@inheritDoc} */
+  @Override
+  public String getText() {
+    return textLabel.getText();
+  }
 
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.google.gwt.user.client.ui.HasText#setText(java.lang.String)
+   */
+  /** {@inheritDoc} */
+  @Override
+  public void setText(String text) {
+    textLabel.setText(text);
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.googlecode.mgwt.ui.client.dialog.HasTitleText#setTitleText(java.lang.String)
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public void setTitleText(String text) {
-		dialogPanel1.getDialogTitle().setText(text);
+  }
 
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.googlecode.mgwt.ui.client.dialog.HasTitleText#setTitleText(java.lang.String)
+   */
+  /** {@inheritDoc} */
+  @Override
+  public void setTitleText(String text) {
+    dialogPanel1.getDialogTitle().setText(text);
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.googlecode.mgwt.ui.client.dialog.HasTitleText#getTitleText()
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public String getTitleText() {
-		return dialogPanel1.getDialogTitle().getText();
-	}
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.googlecode.mgwt.ui.client.dialog.Dialog#show()
-	 */
-	/**
-	 * <p>
-	 * show
-	 * </p>
-	 */
-	public void show() {
-		popinDialog.center();
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.googlecode.mgwt.ui.client.dialog.HasTitleText#getTitleText()
+   */
+  /** {@inheritDoc} */
+  @Override
+  public String getTitleText() {
+    return dialogPanel1.getDialogTitle().getText();
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see com.googlecode.mgwt.dom.client.event.tap.HasTapEvent#addTapHandler(com.googlecode.mgwt.dom.client.event.tap.TapHandler)
-	 */
-	/** {@inheritDoc} */
-	@Override
-	public HandlerRegistration addTapHandler(TapHandler handler) {
-		return dialogPanel1.getOkButton().addTapHandler(handler);
-	}
+  /*
+   * (non-Javadoc)
+   * 
+   * @see com.googlecode.mgwt.ui.client.dialog.Dialog#show()
+   */
+  /**
+   * <p>
+   * show
+   * </p>
+   */
+  public void show() {
+    popinDialog.center();
+  }
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see
+   * com.googlecode.mgwt.dom.client.event.tap.HasTapEvent#addTapHandler(com.googlecode.mgwt.dom.
+   * client.event.tap.TapHandler)
+   */
+  /** {@inheritDoc} */
+  @Override
+  public HandlerRegistration addTapHandler(TapHandler handler) {
+    return dialogPanel1.getOkButton().addTapHandler(handler);
+  }
 
 }
--- src/main/java/com/googlecode/mgwt/ui/client/dialog/DialogPanel.java
+++ src/main/java/com/googlecode/mgwt/ui/client/dialog/DialogPanel.java
@@ -1,16 +1,14 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.ui.client.dialog;
@@ -31,147 +29,156 @@ import com.googlecode.mgwt.ui.client.widget.base.ButtonBase;
  * @author Daniel Kurka
  */
 public class DialogPanel extends Composite {
-	private static class CancelButton extends ButtonBase {
+  private static class CancelButton extends ButtonBase {
 
-		public CancelButton(DialogCss css, String text) {
-			super(css);
-			setText(text);
-			addStyleName(css.cancelbutton());
+    public CancelButton(DialogCss css, String text) {
+      super(css);
+      setText(text);
+      addStyleName(css.cancelbutton());
 
-		}
+    }
 
-	}
+  }
 
-	private static class OkButton extends ButtonBase {
+  private static class OkButton extends ButtonBase {
 
-		public OkButton(DialogCss css, String text) {
+    public OkButton(DialogCss css, String text) {
 
-			super(css);
-			setText(text);
-			addStyleName(css.okbutton());
+      super(css);
+      setText(text);
+      addStyleName(css.okbutton());
 
-		}
+    }
 
-	}
+  }
 
-	private FlowPanel main;
-	private FlowPanel container;
-	private HTML title;
-	private FlowPanel content;
-	private FlowPanel buttonContainer;
-	private OkButton okButton;
-	private CancelButton cancelButton;
-	private final DialogCss css;
+  private FlowPanel main;
+  private FlowPanel container;
+  private HTML title;
+  private FlowPanel content;
+  private FlowPanel buttonContainer;
+  private OkButton okButton;
+  private CancelButton cancelButton;
+  private final DialogCss css;
 
-	/**
-	 * Construct the panel
-	 */
-	public DialogPanel() {
-		this(MGWTStyle.getTheme().getMGWTClientBundle().getDialogCss());
-	}
+  /**
+   * Construct the panel
+   */
+  public DialogPanel() {
+    this(MGWTStyle.getTheme().getMGWTClientBundle().getDialogCss());
+  }
 
-	/**
-	 * Construct panel with a special css
-	 * 
-	 * @param css the css to use
-	 */
-	public DialogPanel(DialogCss css) {
-		this.css = css;
-		this.css.ensureInjected();
-		main = new FlowPanel();
-		initWidget(main);
+  /**
+   * Construct panel with a special css
+   * 
+   * @param css the css to use
+   */
+  public DialogPanel(DialogCss css) {
+    this.css = css;
+    this.css.ensureInjected();
+    main = new FlowPanel();
+    initWidget(main);
 
-		main.addStyleName(css.getDialogPanel());
+    main.addStyleName(css.getDialogPanel());
 
-		container = new FlowPanel();
-		container.addStyleName(css.container());
+    container = new FlowPanel();
+    container.addStyleName(css.container());
 
-		main.add(container);
+    main.add(container);
 
-		title = new HTML();
-		title.addStyleName(css.title());
-		container.add(title);
+    title = new HTML();
+    title.addStyleName(css.title());
+    container.add(title);
 
-		content = new FlowPanel();
-		content.addStyleName(css.content());
-		container.add(content);
+    content = new FlowPanel();
+    content.addStyleName(css.content());
+    container.add(content);
 
-		buttonContainer = new FlowPanel();
-		buttonContainer.addStyleName(css.footer());
-		container.add(buttonContainer);
+    buttonContainer = new FlowPanel();
+    buttonContainer.addStyleName(css.footer());
+    container.add(buttonContainer);
 
-		okButton = new OkButton(css, "Ok");
+    okButton = new OkButton(css, "Ok");
 
-		buttonContainer.add(okButton);
+    buttonContainer.add(okButton);
 
-		cancelButton = new CancelButton(css, "Cancel");
+    cancelButton = new CancelButton(css, "Cancel");
 
-		buttonContainer.add(cancelButton);
+    buttonContainer.add(cancelButton);
 
-	}
+  }
 
-	/**
-	 * get the container of the panel
-	 * 
-	 * @return the container of the dialog panel
-	 */
-	public HasWidgets getContent() {
-		return content;
-	}
+  /**
+   * get the container of the panel
+   * 
+   * @return the container of the dialog panel
+   */
+  public HasWidgets getContent() {
+    return content;
+  }
 
-	/**
-	 * get {@link HasTapHandlers} for the cancel button
-	 * 
-	 * @return the {@link HasTapHandlers} for cancel button
-	 */
-	public HasTapHandlers getCancelButton() {
-		return cancelButton;
-	}
+  /**
+   * get {@link HasTapHandlers} for the cancel button
+   * 
+   * @return the {@link HasTapHandlers} for cancel button
+   */
+  public HasTapHandlers getCancelButton() {
+    return cancelButton;
+  }
 
-	/**
-	 * get {@link HasTapHandlers} for the ok button
-	 * 
-	 * @return the {@link HasTapHandlers} for ok button
-	 */
-	public HasTapHandlers getOkButton() {
-		return okButton;
-	}
+  /**
+   * get {@link HasTapHandlers} for the ok button
+   * 
+   * @return the {@link HasTapHandlers} for ok button
+   */
+  public HasTapHandlers getOkButton() {
+    return okButton;
+  }
 
-	/**
-	 * show the cancel button
-	 * 
-	 * @param show true to show, otherwise hidden
-	 */
-	public void showCancelButton(boolean show) {
-		if (show) {
-			int widgetCount = buttonContainer.getWidgetCount();
-			if (widgetCount == 0) {
-				buttonContainer.add(cancelButton);
-			}
-		} else {
-			buttonContainer.remove(cancelButton);
-		}
-	}
+  public void setOkButtonText(String text) {
+    this.okButton.setText(text);
+  }
 
-	/**
-	 * show the ok button
-	 * 
-	 * @param show true to show, otherwise hidden
-	 */
-	public void showOkButton(boolean show) {
-		if (show) {
-			buttonContainer.insert(okButton, 0);
-		} else {
-			buttonContainer.remove(okButton);
-		}
-	}
+  public void setCancelButtonText(String text) {
+    this.cancelButton.setText(text);
+  }
 
-	/**
-	 * Get the title of the dialog
-	 * 
-	 * @return the title of the dialog
-	 */
-	public HasHTML getDialogTitle() {
-		return title;
-	}
+  /**
+   * show the cancel button
+   * 
+   * @param show true to show, otherwise hidden
+   */
+  public void showCancelButton(boolean show) {
+    if (show) {
+      int widgetCount = buttonContainer.getWidgetCount();
+      if (widgetCount == 0) {
+        buttonContainer.add(cancelButton);
+      }
+    } else {
+      buttonContainer.remove(cancelButton);
+    }
+  }
+
+  /**
+   * show the ok button
+   * 
+   * @param show true to show, otherwise hidden
+   */
+  public void showOkButton(boolean show) {
+    if (show) {
+      buttonContainer.insert(okButton, 0);
+    } else {
+      buttonContainer.remove(okButton);
+    }
+  }
+
+  /**
+   * Get the title of the dialog
+   * 
+   * @return the title of the dialog
+   */
+  public HasHTML getDialogTitle() {
+    return title;
+  }
+
 }
