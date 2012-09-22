2d29176e65e38141bb945cad0c6ad39ffbdcc68a
Daniel Kurka <kurka.daniel@googlemail.com>
2012-09-22 22:38:21 +0200

    fixes ISSUE 250

--- src/main/java/com/googlecode/mgwt/linker/server/propertyprovider/UserAgentPropertyProvider.java
+++ src/main/java/com/googlecode/mgwt/linker/server/propertyprovider/UserAgentPropertyProvider.java
@@ -4,35 +4,35 @@ import javax.servlet.http.HttpServletRequest;
 
 public class UserAgentPropertyProvider extends PropertyProviderBaseImpl {
 
-	/**
+  /**
 	 * 
 	 */
-	private static final long serialVersionUID = 7773351123106881463L;
+  private static final long serialVersionUID = 7773351123106881463L;
 
-	@Override
-	public String getPropertyValue(HttpServletRequest req) throws PropertyProviderException {
+  @Override
+  public String getPropertyValue(HttpServletRequest req) throws PropertyProviderException {
 
-		String userAgent = getUserAgent(req);
+    String userAgent = getUserAgent(req);
 
-		if (userAgent.contains("opera")) {
-			return "opera";
-		}
+    if (userAgent.contains("opera")) {
+      return "opera";
+    }
 
-		if (userAgent.contains("safari")) {
-			return "safari";
-		}
+    if (userAgent.contains("safari") || userAgent.contains("iphone") || userAgent.contains("ipad")) {
+      return "safari";
+    }
 
-		if (userAgent.contains("gecko")) {
-			return "gecko1_8";
-		}
+    if (userAgent.contains("gecko")) {
+      return "gecko1_8";
+    }
 
-		throw new PropertyProviderException("Can not find user agent property for userAgent: '" + userAgent + "'");
+    throw new PropertyProviderException("Can not find user agent property for userAgent: '" + userAgent + "'");
 
-	}
+  }
 
-	@Override
-	public String getPropertyName() {
-		return "user.agent";
-	}
+  @Override
+  public String getPropertyName() {
+    return "user.agent";
+  }
 
 }
