d6fdd4b2df0505d2bef828531b89ae050f7fa28a
Daniel Kurka <kurka.daniel@googlemail.com>
2012-09-24 18:29:55 +0200

    fixed tests

--- src/test/java/com/googlecode/mgwt/test/dom/client/recognizer/MockTouchStartEvent.java
+++ src/test/java/com/googlecode/mgwt/test/dom/client/recognizer/MockTouchStartEvent.java
@@ -1,45 +1,41 @@
 /*
  * Copyright 2012 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.test.dom.client.recognizer;
 
+import com.googlecode.mgwt.collection.shared.CollectionFactory;
 import com.googlecode.mgwt.collection.shared.LightArray;
-import com.googlecode.mgwt.collection.shared.java.JavaLightArray;
 import com.googlecode.mgwt.dom.client.event.touch.Touch;
 import com.googlecode.mgwt.dom.client.event.touch.TouchStartEvent;
 
 public class MockTouchStartEvent extends TouchStartEvent {
 
-	private final int x;
-	private final int y;
-	private final int id;
+  private final LightArray<Touch> touches;
 
-	public MockTouchStartEvent(int id, int x, int y) {
-		this.id = id;
-		this.x = x;
-		this.y = y;
+  public MockTouchStartEvent(int id, int x, int y) {
+    touches = CollectionFactory.constructArray();
+    touches.push(new MockTouch(id, x, y));
 
-	}
+  }
 
-	@Override
-	public LightArray<Touch> getTouches() {
-		JavaLightArray<Touch> array = new JavaLightArray<Touch>();
+  public MockTouchStartEvent(LightArray<Touch> touches) {
+    this.touches = touches;
 
-		array.push(new MockTouch(id, x, y));
+  }
 
-		return array;
-	}
+  @Override
+  public LightArray<Touch> getTouches() {
+    return touches;
+  }
 
 }
