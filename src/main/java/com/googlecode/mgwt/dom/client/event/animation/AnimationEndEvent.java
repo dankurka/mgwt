1bfc4fd9d107422bd126c9532ed7b001c5a1bc3d
Daniel Kurka <kurka.daniel@googlemail.com>
2012-10-26 18:05:19 +0200

    fixes ISSUE 261

--- src/main/java/com/googlecode/mgwt/dom/client/event/animation/AnimationEndEvent.java
+++ src/main/java/com/googlecode/mgwt/dom/client/event/animation/AnimationEndEvent.java
@@ -1,71 +1,75 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.dom.client.event.animation;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.DomEvent;
-import com.googlecode.mgwt.dom.client.event.EventTypes;
+import com.googlecode.mgwt.dom.client.event.EventTypesHolder;
 
 /**
- *
+ * 
  * Represent a native animation end event
- *
+ * 
  * @author Daniel Kurka
  * @version $Id: $
  */
 public class AnimationEndEvent extends DomEvent<AnimationEndHandler> {
-	
-	private static final EventTypes EVENT_TYPES = GWT.create(EventTypes.class);
-	/**
-	 * Event type for animation end events.
-	 */
-	private static final Type<AnimationEndHandler> TYPE = new Type<AnimationEndHandler>(EVENT_TYPES.getAnimationEnd(), new AnimationEndEvent());
 
-	/**
-	 * <p>getType</p>
-	 *
-	 * @return a Type object.
-	 */
-	public static Type<AnimationEndHandler> getType() {
-		return TYPE;
-	}
+  /**
+   * Event type for animation end events.
+   */
+  protected static Type<AnimationEndHandler> TYPE;
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * Gets the event type associated with animation end events.
-	 */
-	@Override
-	public com.google.gwt.event.dom.client.DomEvent.Type<AnimationEndHandler> getAssociatedType() {
-		return TYPE;
-	}
+  /**
+   * <p>
+   * getType
+   * </p>
+   * 
+   * @return a Type object.
+   */
+  public static Type<AnimationEndHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<AnimationEndHandler>(EventTypesHolder.EVENT_TYPES.getAnimationEnd(), new AnimationEndEvent());
+    }
+    return TYPE;
+  }
 
-	/**
-	 * Protected constructor, use
-	 * {@link DomEvent#fireNativeEvent(com.google.gwt.dom.client.NativeEvent, com.google.gwt.event.shared.HasHandlers)}
-	 * to fire animation end events.
-	 */
-	protected AnimationEndEvent() {
+  /**
+   * {@inheritDoc}
+   * 
+   * Gets the event type associated with animation end events.
+   */
+  @Override
+  public com.google.gwt.event.dom.client.DomEvent.Type<AnimationEndHandler> getAssociatedType() {
+    if (TYPE == null) {
+      TYPE = new Type<AnimationEndHandler>(EventTypesHolder.EVENT_TYPES.getAnimationEnd(), new AnimationEndEvent());
+    }
+    return TYPE;
+  }
 
-	}
+  /**
+   * Protected constructor, use
+   * {@link DomEvent#fireNativeEvent(com.google.gwt.dom.client.NativeEvent, com.google.gwt.event.shared.HasHandlers)}
+   * to fire animation end events.
+   */
+  protected AnimationEndEvent() {
 
-	/** {@inheritDoc} */
-	@Override
-	protected void dispatch(AnimationEndHandler handler) {
-		handler.onAnimationEnd(this);
+  }
 
-	}
+  /** {@inheritDoc} */
+  @Override
+  protected void dispatch(AnimationEndHandler handler) {
+    handler.onAnimationEnd(this);
+
+  }
 }
--- src/main/java/com/googlecode/mgwt/dom/client/event/animation/TransitionEndEvent.java
+++ src/main/java/com/googlecode/mgwt/dom/client/event/animation/TransitionEndEvent.java
@@ -1,68 +1,73 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.mgwt.dom.client.event.animation;
 
-import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.DomEvent;
-import com.googlecode.mgwt.dom.client.event.EventTypes;
+import com.googlecode.mgwt.dom.client.event.EventTypesHolder;
 
 /**
- *
+ * 
  * Represent a native transition end event
- *
+ * 
  * @author Daniel Kurka
  * @version $Id: $
  */
 public class TransitionEndEvent extends DomEvent<TransitionEndHandler> {
 
-	private static final EventTypes EVENT_TYPES = GWT.create(EventTypes.class);
-	
-	private static final Type<TransitionEndHandler> TYPE = new Type<TransitionEndHandler>(EVENT_TYPES.getTransistionEnd(), new TransitionEndEvent());
+  protected static Type<TransitionEndHandler> TYPE;
 
-	/**
-	 * <p>getType</p>
-	 *
-	 * @return a Type object.
-	 */
-	public static Type<TransitionEndHandler> getType() {
-		return TYPE;
-	}
+  /**
+   * <p>
+   * getType
+   * </p>
+   * 
+   * @return a Type object.
+   */
+  public static Type<TransitionEndHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<TransitionEndHandler>(EventTypesHolder.EVENT_TYPES.getTransistionEnd(), new TransitionEndEvent());
+    }
+    return TYPE;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * Gets the event type associated with transition end events.
-	 */
-	@Override
-	public com.google.gwt.event.dom.client.DomEvent.Type<TransitionEndHandler> getAssociatedType() {
-		return TYPE;
-	}
+  /**
+   * {@inheritDoc}
+   * 
+   * Gets the event type associated with transition end events.
+   */
+  @Override
+  public com.google.gwt.event.dom.client.DomEvent.Type<TransitionEndHandler> getAssociatedType() {
+    if (TYPE == null) {
+      TYPE = new Type<TransitionEndHandler>(EventTypesHolder.EVENT_TYPES.getTransistionEnd(), new TransitionEndEvent());
+    }
+    return TYPE;
+  }
 
-	/**
-	 * <p>Constructor for TransitionEndEvent.</p>
-	 */
-	protected TransitionEndEvent() {
+  /**
+   * <p>
+   * Constructor for TransitionEndEvent.
+   * </p>
+   */
+  protected TransitionEndEvent() {
 
-	}
+  }
 
-	/** {@inheritDoc} */
-	@Override
-	protected void dispatch(TransitionEndHandler handler) {
-		handler.onTransitionEnd(this);
+  /** {@inheritDoc} */
+  @Override
+  protected void dispatch(TransitionEndHandler handler) {
+    handler.onTransitionEnd(this);
 
-	}
+  }
 
 }
