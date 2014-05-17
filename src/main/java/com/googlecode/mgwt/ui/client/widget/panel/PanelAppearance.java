1110703155d95511cd4cb82af267d90bbedbcfc9
Katharina Fahnenbruck <katharina.fahnenbruck@gmail.com>
2014-05-17 20:38:07 +0200

    Changed panel from CssResource to MGWTCssResource.

--- src/main/java/com/googlecode/mgwt/ui/client/widget/panel/PanelAppearance.java
+++ src/main/java/com/googlecode/mgwt/ui/client/widget/panel/PanelAppearance.java
@@ -1,19 +1,19 @@
 package com.googlecode.mgwt.ui.client.widget.panel;
 
-import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Widget;
+import com.googlecode.mgwt.ui.client.util.MGWTCssResource;
 
 public interface PanelAppearance {
-  public interface PanelCss extends CssResource {
+  public interface PanelCss extends MGWTCssResource {
     @ClassName("mgwt-Panel")
     public String panel();
 
     @ClassName("mgwt-Panel-round")
     public String panelRound();
   }
-  
+
   PanelCss css();
-  
+
   UiBinder<Widget, Panel> uiBinder();
 }
