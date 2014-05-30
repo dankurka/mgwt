beb902299c1c4221aa72c1fdcf018ea8c74c3cde
Katharina Fahnenbruck <katharina.fahnenbruck@gmail.com>
2014-05-30 19:10:55 +0200

    Add place lookup to HistoryHandler.

--- src/main/java/com/googlecode/mgwt/mvp/client/history/HistoryHandler.java
+++ src/main/java/com/googlecode/mgwt/mvp/client/history/HistoryHandler.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2011 Daniel Kurka
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -25,4 +25,6 @@ public interface HistoryHandler {
 	public void goTo(Place place);
 
 	public void goTo(Place place, boolean ignore);
+
+  public Place getWhere();
 }
